16bfe8578817fa59338e8d156deb5403fd5ce0cf
Andrea Piccione <piccione@diag.uniroma1.it>
2022-07-26 05:05:09 +0200

    rootsimcc works.
    A lot of stuff has been removed to make things work. Major changes:
    - setup a git submodule for the ROOT-Sim core library
    - our custom compiler is now written in a very simple C++ style
    - handmade vectors and dynamic strings removed
    - the basic llvm plugin that does function copy and substitution
      works with llvm 14, with both the legacy pass manager and the new one
    - CMake magic is now in place to be able to contextually build the core
      library and install it together with the compiler, the rootsimcc
      library and the llvm plugins
    
    This thing incredibly works with a phold model.
    TODO: make things ACTUALLY work
    
    Criticism and suggestions are always welcome!
    
    Signed-off-by: Andrea Piccione <piccione@diag.uniroma1.it>

--- test/compiler.c
+++ test/compiler.c
@@ -6,8 +6,6 @@
  * SPDX-FileCopyrightText: 2008-2021 HPDCS Group <rootsim@googlegroups.com>
  * SPDX-License-Identifier: GPL-3.0-only
  */
-#include <test.h>
-
 // FIXME this test is totally dummy and useless
 
 int main(void)
